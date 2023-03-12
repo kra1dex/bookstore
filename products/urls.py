c66f183e2f43deeb1e1a1bf6b55cfea408db457d
Grisha <grishamalanchuk@gmail.com>
2023-03-12 23:51:01 +0200

    genre viewset

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from products import views
+
+router = routers.SimpleRouter()
+router.register('genres', views.GenreViewSet)
 
 urlpatterns = [
     path('', views.BookListView.as_view()),
     path('create/', views.BookCreateView.as_view()),
+    path('', include(router.urls)),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.viewsets import ModelViewSet
 
-from products.models import Book
-from products.serializers import BookListSerializer, BookCreateSerializer
+from products.models import Book, Genre
+from products.serializers import BookListSerializer, BookCreateSerializer, GenreSerializer
 
 
 class BookListView(ListAPIView):
@@ -12,3 +13,8 @@ class BookListView(ListAPIView):
 class BookCreateView(CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookCreateSerializer
+
+
+class GenreViewSet(ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
